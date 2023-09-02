05888122a95ca79e7eb3ea5dd755cd78dc1f4aa9
Akuna444 <aklilsolomon3@gmail.com>
2023-09-02 20:22:18 +0300

    add: pricing page

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+import { BrainCircuit } from "lucide-react";
+
+type Props = {};
+
+const QuizMeCard = (props: Props) => {
+  const router = useRouter();
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default QuizMeCard;
