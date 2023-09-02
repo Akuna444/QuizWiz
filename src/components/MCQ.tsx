5a398ee8b2cf24c8f524c5d03ddfdc75cb707855
Akuna444 <aklilsolomon3@gmail.com>
2023-09-02 20:22:07 +0300

    update: navbar components

--- src/components/MCQ.tsx
+++ src/components/MCQ.tsx
@@ -151,7 +151,7 @@ const MCQ = ({ game }: Props) => {
   }
 
   return (
-    <div className="absolute -translate-x-1/2 -translate-y-1/2 md:w-[80vw] max-w-4xl w-[90vw] top-1/2 left-1/2">
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 md:w-[80vw] max-w-4xl w-[90vw] top-[60%] left-1/2">
       <div className="flex flex-row justify-between">
         <div className="flex flex-col">
           {/* topic */}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,15 +10,40 @@ import SignInButton from "./SignInButton";
 const Navbar = async () => {
   const session = await getAuthSession();
   return (
-    <div className="fixed inset-x-0 top-0 bg-[#4ECDC4] dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300  py-2 ">
+    <div className="fixed inset-x-0 top-0 bg-[#4ECDC4] z-[10] dark:bg-gray-950 h-fit border-b border-zinc-300  py-2 ">
       <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
         {/* Logo */}
         <Link href={"/"} className="flex items-center gap-2">
-          <Image src="/logo.png" alt="logo" height={80} width={80} />
+          <Image src="/logo.png" alt="logo" height={60} width={60} />
           <p className="rounded-lg  text-white  px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
             QuizWiz
           </p>
         </Link>
+        <div className="flex text-white gap-3">
+          <Link href="/quiz" className="flex gap-2">
+            <Image width={60} height={60} src="/quiz.png" alt="quiz icon" />
+            <div className="flex h-full items-center">
+              <h1>Quizzes</h1>
+            </div>
+          </Link>
+          <Link href="#" className="flex gap-2">
+            <Image
+              width={60}
+              height={60}
+              src="/price.png"
+              alt="pricetag icon"
+            />
+            <div className="flex h-full items-center">
+              <h1>Pricing</h1>
+            </div>
+          </Link>
+          <Link href="#" className="flex gap-2">
+            <Image width={60} height={60} src="/demo.png" alt="demo icon" />
+            <div className="flex h-full items-center">
+              <h1>Demo</h1>
+            </div>
+          </Link>
+        </div>
         <div className="flex items-center">
           <ThemeToggle className="mr-4" />
           {session?.user ? (
